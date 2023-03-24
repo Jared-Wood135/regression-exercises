2dd839729544e986ea7b9eec68a5884a0ac84979
Jared Wood <jared.wood135@gmail.com>
2023-03-24 11:34:16 -0500

    .

---
+++ wrangle.py
@@ -0,0 +1,44 @@
+# =======================================================================================================
+# Table of Contents START
+# =======================================================================================================
+
+'''
+1. Orientation
+2. Imports
+3. 
+'''
+
+# =======================================================================================================
+# Table of Contents END
+# Table of Contents TO Orientation
+# Orientation START
+# =======================================================================================================
+
+'''
+The purpose of this file is to create functions for both the acquire & preparation phase of the data
+science pipeline or also known as 'wrangle' data...
+'''
+
+# =======================================================================================================
+# Orientation END
+# Orientation TO Imports
+# Imports START
+# =======================================================================================================
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy import stats
+
+# =======================================================================================================
+# Imports END
+# Imports TO 
+#  START
+# =======================================================================================================
+
+def func():
+    '''
+    Docstring here
+    '''
+    print('placeholder')
