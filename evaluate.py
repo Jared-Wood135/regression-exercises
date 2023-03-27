13030d63ec4f47d0cab6537050ee08e60e871791
Jared Wood <jared.wood135@gmail.com>
2023-03-27 14:55:35 -0500

    Created templates for later use when conducting 'evaluate' exercises

---
+++ evaluate.py
@@ -0,0 +1,41 @@
+# =======================================================================================================
+# Table of Contents START
+# =======================================================================================================
+
+'''
+1. Orientation
+2. Imports
+3. 
+'''
+
+# =======================================================================================================
+# Table of Contents END
+# Table of Contents TO Orientation
+# Orientation START
+# =======================================================================================================
+
+'''
+The purpose of this file is to create functions for evaluation purposes
+'''
+
+# =======================================================================================================
+# Orientation END
+# Orientation TO Imports
+# Imports START
+# =======================================================================================================
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import itertools
+from scipy import stats
+from sklearn.model_selection import train_test_split
+import os
+import wrangle
+
+# =======================================================================================================
+# Imports END
+# Imports TO 
+#  START
+# =======================================================================================================
